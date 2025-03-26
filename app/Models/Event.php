18cd5947063024e2fc0e259a16ef77cc4f5fd61c
Julian Derksen <191808@novacollege.nl>
2025-03-26 08:53:23 +0100

    created models

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,5 +6,88 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    protected $table = 'events';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'end_date',
+        'is_virtual',
+        'venue_id',
+        'organizer_id',
+        'status',
+    ];
+
+    // Relatie met Venue
+    public function venue()
+    {
+        return $this->belongsTo(Venue::class);
+    }
+
+    // Relatie met Organizer (als dit een gebruiker is, pas dan de modelnaam aan)
+    public function organizer()
+    {
+        return $this->belongsTo(User::class, 'organizer_id');
+    }
+
+    // Tickets
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    // Sessions
+    public function sessions()
+    {
+        return $this->hasMany(Session::class);
+    }
+
+    // Surveys
+    public function surveys()
+    {
+        return $this->hasMany(Survey::class);
+    }
+
+    // Budgets
+    public function budgets()
+    {
+        return $this->hasMany(Budget::class);
+    }
+
+    // Marketing Campaigns
+    public function marketingCampaigns()
+    {
+        return $this->hasMany(MarketingCampaign::class);
+    }
+
+    // Resources
+    public function resources()
+    {
+        return $this->hasMany(Resource::class);
+    }
+
+    // EventStaff
+    public function staff()
+    {
+        return $this->hasMany(EventStaff::class);
+    }
+
+    // Sponsors (many-to-many)
+    public function sponsors()
+    {
+        return $this->belongsToMany(Sponsor::class);
+    }
+
+    // Exhibitors (many-to-many)
+    public function exhibitors()
+    {
+        return $this->belongsToMany(Exhibitor::class);
+    }
+
+    // Communications
+    public function communications()
+    {
+        return $this->hasMany(Communication::class);
+    }
 }
